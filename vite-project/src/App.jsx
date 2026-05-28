946b6df319a3b50d7db39918d031a4ba618e46c8
L3ticias0uz4 <leticiasouza56000@gmail.com>
2026-05-28 11:11:13 -0300

    feat: add initial project structure with components for header, card, and main sections
    - Added Card component with styles for character display
    - Created Header component with title and description
    - Implemented Main component to render multiple Card components
    - Included SVG assets for React and Vite logos
    - Configured Vite for React application

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Main } from './components/main'
+import { Header } from './components/header'
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Main/>
+     </>
+  )
+}
+export default App
---
+++ vite-project/src/components/main/index.jsx
@@ -0,0 +1,15 @@
+import { Card } from '../card'
+import './styles.css'
+export const Main = () => {
+    return(
+    <main>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/> 
+              
+    </main>
+    )
+}
