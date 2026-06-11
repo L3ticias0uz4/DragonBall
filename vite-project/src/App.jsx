8af8a9757a6e0eb0eaae0dfd908c1688cd10a4e1
L3ticias0uz4 <leticiasouza56000@gmail.com>
2026-06-11 11:10:41 -0300

    feat: implement user authentication with login and profile retrieval

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,56 @@
 import './App.css'
-import { Main } from './components/main'
-import { Header } from './components/header'
+import { useState } from 'react'
+
 function App() {
 
+  const[email,setEmail] = useState("")
+  const[senha,setSenha] = useState("")
+  const[usuario,setUsuario] = useState(null)
+
+  const login = async() => {
+
+    try{
+    const resposta = await  axios.post("http://localhost:3000/login",
+      {
+        email, senha
+      }
+    )
+    const { token } = resposta.data.token
+
+    localStorage.setItem("token", token)
+    alert("Login realizado")
+    }
+    catch{
+      alert("Login invalido")
+    }
+  }
+
+  const buscar = async () => {
+    const token = localStorage.getItem("token")
+
+    const resposta = await axios.get("http://localhost:3000/perfil",{
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }
+    )
+    
+    setUsuario(resposta.data.usuario)  }
   return (
     <>
-      <Header/>
-      <Main/>
+      <h2>login</h2>
+
+      <input type='email' placeholder='Informe seu email'
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input type='password' placeholder='Informe sua senha'
+      value={senha}
+      onChange={(e) => setSenha(e.target.value)}
+      />
+
+      <button onClick={login}>Entrar</button>
      </>
   )
 }
